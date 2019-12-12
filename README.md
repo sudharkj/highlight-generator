7e5dbc4213bc930027c2b8e1a7ec41c59bbb4877
J S Ravi Kumar <sudha.rkj@gmail.com>
2019-12-11 21:26:02 -0800

    Add generate link (#3)
    * add rest api to generate scored images
    * for now allow file download from static links
    * add requirements file and the corresponding installation documentation

--- README.md
+++ README.md
@@ -1,1 +1,15 @@
-Highlight Generator
+## Highlight Generator
+
+#### Getting Started
+
+###### Install with conda
+
+```shell script
+conda install --file conda-requirements.txt
+```
+
+###### Install with pip
+
+```shell script
+pip install -r pip-requirements.txt
+```
--- app.py
+++ app.py
@@ -1,3 +1,4 @@
+import requests
 from flask import Flask
 
 app = Flask(__name__)
@@ -5,6 +6,21 @@ app = Flask(__name__)
 
 @app.route('/')
 def hello_world():
+    return 'Hello World!'
+
+
+@app.route('/generate')
+def generate_highlights():
+    url_link = 'junk'
+    path = 'downloads/random.mp4'
+    chunk_size = 8192
+
+    with requests.get(url_link, stream=True) as r:
+        with open(path, 'wb') as out:
+            for chunk in r.iter_content(chunk_size=chunk_size):
+                if chunk:
+                    out.write(chunk)
+
     return 'Hello World!'
 
 
