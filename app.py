e4b43dfa2fe8c240e7c4b4465444c018e6542eef
J S Ravi Kumar <sudha.rkj@gmail.com>
2019-12-11 21:45:10 -0800

    Split downloaded video to max 100 frames (#4)
    * go through each of the frames
    * split them at max 100 equi-distance frames
    * save them locally for further processing

--- app.py
+++ app.py
@@ -1,3 +1,6 @@
+import math
+
+import cv2
 import requests
 from flask import Flask
 
@@ -20,6 +23,27 @@ def generate_highlights():
             for chunk in r.iter_content(chunk_size=chunk_size):
                 if chunk:
                     out.write(chunk)
+
+    MAX_FRAMES = 100
+    MIN_TIME_STAMP = 5
+    frame_skip_count = 1
+
+    video_cap = cv2.VideoCapture(path)
+    fps = video_cap.get(cv2.CAP_PROP_FPS)
+    frame_count = video_cap.get(cv2.CAP_PROP_FRAME_COUNT)
+    duration = math.ceil(frame_count / fps)
+    time_skip = max(MIN_TIME_STAMP, math.ceil(duration / MAX_FRAMES))
+    frame_skip_count = max(frame_skip_count, math.ceil(time_skip * fps))
+
+    images_path = 'images'
+    success, image = video_cap.read()
+    count = 1
+    while success:
+        if count % frame_skip_count == 0:
+            cv2.imwrite("%s/frame_%d.jpg" % (images_path, int(count / frame_skip_count)), image)
+        success, image = video_cap.read()
+        count += 1
+    video_cap.release()
 
     return 'Hello World!'
 
