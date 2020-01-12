a641c383480c553d730b74d5712765a1642e415b
J S Ravi Kumar <sudha.rkj@gmail.com>
2020-01-12 11:52:40 -0800

    Return highlights in the order of occurrence (#12)
    * change image name format to include its timestamp from the video
    * return highlights in the order of occurrence
    * send image timestamp in the response
    * change folders path location
    * use .format instead of % formatting in functions where code is modified
    
    Resolves highlight-extractor/highlight-generator#9

--- app.py
+++ app.py
@@ -1,6 +1,7 @@
 import json
 import math
 import os
+import re
 
 import cv2
 from flask import Flask, send_from_directory, request
@@ -11,9 +12,9 @@ import utils
 
 app = Flask(__name__, static_url_path='')
 
-VIDEOS_BASE_PATH = '/Users/sudharkj/Developer/python/highlight-generator/videos'
-IMAGES_BASE_PATH = '/Users/sudharkj/Developer/python/highlight-generator/images'
-PREDICTIONS_BASE_PATH = '/Users/sudharkj/Developer/python/highlight-generator/predictions'
+VIDEOS_BASE_PATH = '/Users/sudharkj/Developer/flask/highlight-generator/videos'
+IMAGES_BASE_PATH = '/Users/sudharkj/Developer/flask/highlight-generator/images'
+PREDICTIONS_BASE_PATH = '/Users/sudharkj/Developer/flask/highlight-generator/predictions'
 
 ALLOWED_EXTENSIONS = {'mov', 'mp4'}
 
@@ -29,9 +30,11 @@ def send_image(path):
 
 
 def generate_output_item(prediction, request_uuid, file_ext):
+    timestamp = [int(s) for s in re.findall(r'\d+', prediction['image_id'])][0]
     return {
-        'image_id': "/images/%s/%s.%s" % (request_uuid, prediction['image_id'], file_ext),
-        'mean_score_prediction': prediction['mean_score_prediction']
+        'image_id': "/images/{}/{}.{}".format(request_uuid, prediction['image_id'], file_ext),
+        'mean_score_prediction': prediction['mean_score_prediction'],
+        'timestamp': timestamp
     }
 
 
@@ -51,9 +54,10 @@ def generate_highlights():
     WEIGHTS_FILE = 'weights_mobilenet_aesthetic_0.07.hdf5'
 
     request_uid = utils.rand_gen()
-    video_path = VIDEOS_BASE_PATH + '/' + request_uid
-    images_path = IMAGES_BASE_PATH + '/' + request_uid
-    predictions_path = PREDICTIONS_BASE_PATH + '/' + request_uid
+    path_format = '{}/{}'
+    video_path = path_format.format(VIDEOS_BASE_PATH, request_uid)
+    images_path = path_format.format(IMAGES_BASE_PATH, request_uid)
+    predictions_path = path_format.format(PREDICTIONS_BASE_PATH, request_uid)
 
     for dir_path in [video_path, images_path, predictions_path]:
         if not os.path.exists(dir_path):
@@ -68,7 +72,7 @@ def generate_highlights():
     video_name = secure_filename(file.filename)
     video_file_path = os.path.join(video_path, video_name)
     file.save(video_file_path)
-    print('Saved file at %s' % video_file_path)
+    print('Saved file at {}'.format(video_file_path))
 
     video_cap = cv2.VideoCapture(video_file_path)
     fps = video_cap.get(cv2.CAP_PROP_FPS)
@@ -76,28 +80,31 @@ def generate_highlights():
     duration = math.ceil(frame_count / fps)
     time_skip = max(MIN_TIME_STAMP, math.ceil(duration / MAX_FRAMES))
     frame_skip_count = max(frame_skip_count, math.ceil(time_skip * fps))
-    print('Sampling the video after %d frames' % frame_skip_count)
+    print('Sampling the video after {} frames'.format(frame_skip_count))
 
     success, image = video_cap.read()
+    timestamp = int(video_cap.get(cv2.CAP_PROP_POS_MSEC))
     count = 1
     while success:
         if count % frame_skip_count == 0:
-            cv2.imwrite("%s/frame_%d.%s" % (images_path, int(count / frame_skip_count), file_ext), image)
+            cv2.imwrite("{}/frame_{}.{}".format(images_path, timestamp, file_ext), image)
         success, image = video_cap.read()
+        timestamp = int(video_cap.get(cv2.CAP_PROP_POS_MSEC))
         count += 1
     video_cap.release()
     print('Completed sampling and saving the frames')
 
-    predictions_file = '%s/predicts.json' % predictions_path
-    print('Running %s to predict the scores of the sampled frames' % BASE_MODEL)
+    predictions_file = '{}/predicts.json'.format(predictions_path)
+    print('Running {} to predict the scores of the sampled frames'.format(BASE_MODEL))
     predictions = predict.score(BASE_MODEL, WEIGHTS_FILE, images_path, predictions_file)
     print('Completed prediction of frames')
     predictions = sorted(predictions, key=lambda k: k['mean_score_prediction'], reverse=True)
     predictions = predictions[:PREDICTIONS_LIMIT]
-    print('Top %d Results:' % PREDICTIONS_LIMIT)
+    print('Top {} Results:'.format(PREDICTIONS_LIMIT))
     print(json.dumps(predictions, indent=2))
     predictions = list(map(lambda prediction: generate_output_item(prediction, request_uid, file_ext),
                            predictions))
+    predictions = sorted(predictions, key=lambda k: k['timestamp'])
     print('Response:')
     print(json.dumps(predictions, indent=2))
 
